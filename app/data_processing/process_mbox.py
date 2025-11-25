e54d6a77a5fa6928c452b8588fba3b1f1c88c17a
Latandu <kuba.starogard@gmail.com>
2025-11-25 17:47:29 +0100

    Remove process_mbox.py script and add empty readme.md in output directory

--- app/data_processing/process_mbox.py
+++ app/data_processing/process_mbox.py
@@ -13,13 +13,13 @@ for i in range(2024, 2012, -1):
 final_df = pl.concat(dfs)
 fe.write_to_file(final_df, "emails_phishing_jose.parquet")
 csv_configs = [
-    {"input_csv": "csv_misc/TREC-05.csv", "output_parquet": "trec_05.parquet"},
-    {"input_csv": "csv_misc/TREC-06.csv", "output_parquet": "trec_06.parquet"},
-    {"input_csv": "csv_misc/Enron.csv", "output_parquet": "enron.parquet"},
-    {"input_csv": "csv_misc/TREC-07.csv", "output_parquet": "trec_07.parquet"},
-    {"input_csv": "csv_misc/CEAS-08.csv", "output_parquet": "ceas_08.parquet"},
-    {"input_csv": "csv_misc/Ling.csv", "output_parquet": "ling.parquet"},
-    {"input_csv": "csv_misc/Assassin.csv", "output_parquet": "assassin.parquet"}
+    {"input_csv": "data/csv_misc/TREC-05.csv", "output_parquet": "trec_05.parquet"},
+    {"input_csv": "data/csv_misc/TREC-06.csv", "output_parquet": "trec_06.parquet"},
+    {"input_csv": "data/csv_misc/Enron.csv", "output_parquet": "enron.parquet"},
+    {"input_csv": "data/csv_misc/TREC-07.csv", "output_parquet": "trec_07.parquet"},
+    {"input_csv": "data/csv_misc/CEAS-08.csv", "output_parquet": "ceas_08.parquet"},
+    {"input_csv": "data/csv_misc/Ling.csv", "output_parquet": "ling.parquet"},
+    {"input_csv": "data/csv_misc/Assassin.csv", "output_parquet": "assassin.parquet"}
 ]
 results = fe.process_multiple_csvs(csv_configs)
 final_df = pl.concat(results)
@@ -27,17 +27,17 @@ fe.write_to_file(final_df, "emails_csv.parquet")
 
 
 dfs = []
-directory = os.fsencode("mbox_final_leg")
+directory = os.fsencode("data/mbox_final_leg")
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".mbox"): 
-        df = fe.process_mbox(f"mbox_final_leg/{filename}", f"{filename}", phishing=False)
+        df = fe.process_mbox(f"data/mbox_final_leg/{filename}", f"{filename}", phishing=False)
         dfs.append(df)
     else:
         continue
 final_df = pl.concat(dfs)
 fe.write_to_file(final_df, "emails_gmail.parquet")
 
-df = fe.process_mbox("fradulent_emails.mbox", f"phishing-nigerian", phishing=True)
+df = fe.process_mbox("data/fradulent_emails.mbox", f"phishing-nigerian", phishing=True)
 print(df)
 fe.write_to_file(df, f"phishing_nigerian.parquet")
